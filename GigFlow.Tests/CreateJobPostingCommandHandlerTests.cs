361df453d94ea142b54ed1348d0459c6c8871068
Meric Beser <mebeser1@gmail.com>
2026-04-01 11:47:27 +0300

    Add unit tests for CreateJobPosting handler and validator

---
+++ GigFlow.Tests/CreateJobPostingCommandHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using GigFlow.Domain.Entities;
+using GigFlow.Application.Features.JobPostings.Commands.CreateJobPosting;
+using GigFlow.Application.Repositories;
+
+using Moq;
+namespace GigFlow.Tests;
+
+public class CreateJobPostingCommandHandlerTests
+{
+    [Fact]
+    public async Task Handle_ShouldReturnGuid_WhenJobPostingCreated()
+    {
+        
+        var mockRepo = new Mock<IJobPostingRepository>();
+        var handler = new CreateJobPostingCommandHandler(mockRepo.Object);
+
+        var command = new CreateJobPostingCommand
+        {
+            Title = "Test Job",
+            Description = "Test Description",
+            CategoryId = Guid.NewGuid(),
+            BudgetMin = 1000,
+            BudgetMax = 5000,
+            ClientId = Guid.NewGuid(),
+            Deadline = DateTime.UtcNow.AddDays(30),
+            SkillIds = new List<Guid>()
+        };
+        var result = await handler.Handle(command, CancellationToken.None);
+        
+        //Assert
+        Assert.NotEqual(Guid.Empty,result);
+        mockRepo.Verify(x=> x.AddAsync(It.IsAny<JobPosting>()), Times.Once);
+    }
+
+   
+}
---
+++ GigFlow.Tests/CreateJobPostingCommandValidatorTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation.TestHelper;
+using GigFlow.Application.Features.JobPostings.Commands.CreateJobPosting;
+
+namespace GigFlow.Tests;
+
+public class CreateJobPostingCommandValidatorTests
+{
+    private readonly CreateJobPostingCommandValidator _validator = new();
+
+    [Fact]
+    public void Title_WhenEmpty_ShouldHaveError()
+    {
+        var command = new CreateJobPostingCommand { Title = "" };
+        var result = _validator.TestValidate(command);
+        result.ShouldHaveValidationErrorFor(x => x.Title);
+    }
+
+    [Fact]
+    public void BudgetMax_WhenLessThanBudgetMind_ShouldHaveError()
+    {
+        var command = new CreateJobPostingCommand
+        {
+            BudgetMin = 5000,
+            BudgetMax = 1000
+        };
+        var result = _validator.TestValidate(command);
+        result.ShouldHaveValidationErrorFor(x => x.BudgetMax);
+    }
+
+    [Fact]
+    public void DeadLine_WhenBeforeDate_ShouldHaveError()
+    {
+        var command = new CreateJobPostingCommand {Deadline = DateTime.Now.AddDays(-1)};
+        var result = _validator.TestValidate(command);
+        result.ShouldHaveValidationErrorFor(x => x.Deadline);
+    }
+
+    [Fact]
+    public void SkıllIds_WhenEmpty_ShouldHaveError()
+    {
+        var command = new CreateJobPostingCommand { SkillIds = null };
+        var result = _validator.TestValidate(command);
+        result.ShouldHaveValidationErrorFor(x => x.SkillIds);
+    }
+}
